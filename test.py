82cf2648b8db92c84ea3707e1d9aa63a235a2ac4
bellac5 <60378948+bellac5@users.noreply.github.com>
2024-04-10 13:49:06 -0600

    .

---
+++ test.py
@@ -0,0 +1,44 @@
+levels = {'1': ['mom', 'dad', 'parents'], 
+          '2': ['bird', 'plane', 'fly'], 
+          '3': ['heel', 'bark', 'dog'],
+          '4': ['tree', 'glue', 'stick'],
+          '5': ['theme', 'swing', 'park'],
+          '6': ['football', 'smartphone', 'screen']}
+
+curr_level = 1
+
+for i in range(len(levels)):
+  lives = 5
+  if lives <= 0:
+    break
+  else:
+    # initialize level
+    lives = 5
+    print(f'-- LEVEL {curr_level} --')
+    print()
+    words = levels.get(str(curr_level))
+    print(f'{words[0]}     {words[1]}')
+    print()
+    print(f'LINK is {len(words[2])} characters long:')
+    print()
+    ans = input()
+
+    # incorrect
+    while ans != words[2]:
+      lives -= 1
+      print(f'incorrect. you have {lives} lives left.')
+      print()
+      # checking if they can continue to play
+      if lives == 0:
+        print('*** game over ***')
+        break
+      else:
+        ans = input()
+
+    # correct   
+    if ans == words[2]:
+      print()
+      print("*** correct! onto the next level ***")
+      print()
+      curr_level += 1
+    
