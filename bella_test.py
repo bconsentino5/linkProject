0a5118e995e1907a40f14d44d67ab03d70c47098
bellac5 <60378948+bellac5@users.noreply.github.com>
2024-04-24 13:47:03 -0600

    Update bella_test.py

--- bella_test.py
+++ bella_test.py
@@ -191,25 +191,25 @@ WORDS = response.content.split()
 
 
 levels = {'1': ['mom', 'dad', 'parents'], 
-          '2': ['bird', 'plane', 'fly']} 
-          # '3': ['heel', 'bark', 'dog'],
-          # '4': ['tree', 'glue', 'stick'],
-          # '5': ['theme', 'swing', 'park'],
-          # '6': ['football', 'smartphone', 'screen'],
-          # '7': ['computer', 'door', 'key'],
-          # '8': ['technology', 'fruit', 'apple'],
-          # '9': ['trust', 'religion', 'faith'],
-          # '10': ['animal', 'pooper', 'party'],
-          # '11': ['wind', 'string', 'instrument'],
-          # '12': ['bowtie', 'wheel', 'pasta'],
-          # '12': ['dog', 'chair', 'sit'],
-          # '14': ['pumpkin', 'slipper', 'cinderella'],
-          # '15': ['cheese', 'color', 'wheel'],
-          # '16': ['shoe', 'spirit', 'soul'],
-          # '17': ['leap', 'amphibian', 'frog'],
-          # '18': ['slice', 'italy', 'pizza'],
-          # '19': ['heart', 'music', 'beat'],
-          # '20': ['dairy', 'nut', 'butter']}
+          '2': ['bird', 'plane', 'fly'], 
+          '3': ['heel', 'bark', 'dog'],
+          '4': ['tree', 'glue', 'stick'],
+          '5': ['theme', 'swing', 'park'],
+          '6': ['football', 'smartphone', 'screen'],
+          '7': ['computer', 'door', 'key'],
+          '8': ['technology', 'fruit', 'apple'],
+          '9': ['trust', 'religion', 'faith'],
+          '10': ['animal', 'pooper', 'party'],
+          '11': ['wind', 'string', 'instrument'],
+          '12': ['bowtie', 'wheel', 'pasta'],
+          '13': ['dog', 'chair', 'sit'],
+          '14': ['pumpkin', 'slipper', 'cinderella'],
+          '15': ['cheese', 'color', 'wheel'],
+          '16': ['shoe', 'spirit', 'soul'],
+          '17': ['leap', 'amphibian', 'frog'],
+          '18': ['slice', 'italy', 'pizza'],
+          '19': ['heart', 'music', 'beat'],
+          '20': ['dairy', 'nut', 'butter']}
 
 curr_level = 1
 lives = 5
@@ -231,16 +231,26 @@ while lives != 0:
 
     # incorrect
     while ans != words[2]:
-      lives -= 1
-      print(f'incorrect. you have {lives} lives left.')
-      print()
-      # checking if they can continue to play
-      if lives == 0:
-        print(f'>> the LINK was {words[2]} <<')
-        print('*** game over ***')
-        break
+      if len(ans) != len(words[2]):
+         print(f'answer is the wrong length. LINK is {len(words[2])} characters long. try again:')
+         ans = input()
       else:
-        ans = input()
+        lives -= 1
+        for i in range(len(words[2])):
+          if words[2][i] == ans[i]:
+              print(words[2][i], end="")
+          else:
+              print("_", end="")
+        print()
+        print(f'incorrect. you have {lives} lives left.')
+        print()
+        # checking if they can continue to play
+        if lives == 0:
+          print(f'>> the LINK was {words[2]} <<')
+          print('*** game over ***')
+          break
+        else:
+          ans = input()
 
     # correct   
     if ans == words[2]:
@@ -249,7 +259,7 @@ while lives != 0:
       print()
       curr_level += 1
       temp = 0
-      if curr_level > 2:
+      if curr_level > 20:
         while temp == 0:
             curr_word = random.choice(WORDS).decode('utf-8')
             if len(curr_word) < 8:
