7e43d081037f609c80ca5c60bb8f413deae6d39a
bellac5 <60378948+bellac5@users.noreply.github.com>
2024-05-04 20:19:35 -0600

    Update README.py

--- README.py
+++ README.py
@@ -1,7 +1,27 @@
 ''' 
 READ ME
 
-how to download everything for the game
+how to download everything for the game:
+- if you're running python version 3:
+    - to install pygame 
+        - type "python3 -m pip3 install -U pygame --user" in the terminal
+    - to install nltk for the word randomization aspect of the program
+        - type "pip3 install --user -U nltk"
+        - after that is done installing, type "-m nltk.downloader wordnet"
+- if you're running other versions of python:
+    - to install pygame 
+        - type "python -m pip install -U pygame --user" in the terminal
+    - to install nltk for the word randomization aspect of the program
+        - type "pip install --user -U nltk"
+        - after that is done installing, type "-m nltk.downloader wordnet"
+
+to start the game:
+- open combo.py and link_game.py
+- press the play button in VS Code to run the program
+- when you want to quit the game, either press the red "x" on the game screen
+    or if that doesn't work, press control "c" in the terminal
+- if you want to shorten the amount of hard 
+    
 
 
 '''
