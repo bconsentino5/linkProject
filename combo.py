e76a423cbcb1ffce014885b8f7b93f44dcdb6547
MathildeSumner <166655245+MathildeSumner@users.noreply.github.com>
2024-05-04 20:21:11 -0600

    z

--- combo.py
+++ combo.py
@@ -6,7 +6,11 @@ word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
 response = requests.get(word_site)
 WORDS = response.content.split()
 
-max_levels = 2
+'''
+how many levels of the hardcoded levels run before randomized 
+words from library start to apear
+'''
+max_levels = 2 
 levels = {'1': ['mom', 'dad', 'parents'], 
           '2': ['bird', 'plane', 'fly'],
           '3': ['heel', 'bark', 'dog'],
